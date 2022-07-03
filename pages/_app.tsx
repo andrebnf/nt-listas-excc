42254e6c7864ff580eae9a5f70d9c2d1ba83d708
Andre Bonfatti <andrebnf@gmail.com>
2022-07-03 13:54:50 -0300

    added server styled-comp render and moved theme to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from '../styles/global'
+import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import type { NextPage } from 'next';
-import { GlobalStyle } from '../styles/global';
-import { theme } from '../styles/theme';
 import styled from 'styled-components';
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const MySection = styled.section`
   border: 2px solid red;
@@ -11,14 +8,9 @@ const MySection = styled.section`
 
 const Home: NextPage = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <>
-        <GlobalStyle/>
-        <MySection>
-            ola
-        </MySection>
-      </>
-    </ThemeProvider>
+    <MySection>
+        ola
+    </MySection>
   )
 }
 
