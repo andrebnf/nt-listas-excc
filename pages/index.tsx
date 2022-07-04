24d830a946780047d5f51624ef8751047de3c26e
Andre Bonfatti <andrebnf@gmail.com>
2022-07-04 12:46:52 -0300

    added simple react editor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
+import { useState } from 'react';
 import type { NextPage } from 'next';
 import styled from 'styled-components';
+import Editor from 'react-simple-code-editor';
+
+import { highlight, languages } from 'prismjs';
+
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/themes/prism.css'; //Example style, you can use another
+
 import { ExerciseDetails } from '../components/exerciseDetails';
 import { ExerciseCode } from '../components/exerciseCode';
 import { Sidebar } from '../components/sidebar';
@@ -12,11 +21,26 @@ const PageContainer = styled.div`
 `
 
 const Home: NextPage = () => {
+  const [code, setCode] = useState(
+    `function add(a, b) {\n  return a + b;\n}`
+  );
+
   return (
     <PageContainer>
       <Sidebar></Sidebar>
       <ExerciseDetails></ExerciseDetails>
-      <ExerciseCode></ExerciseCode>
+      <div>
+          <Editor
+          value={code}
+          onValueChange={code => setCode(code)}
+          highlight={code => highlight(code, languages.js, 'javascript')}
+          padding={10}
+          style={{
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            fontSize: 24,
+          }}
+        />
+      </div>
     </PageContainer>
   )
 }
