4d2359ae4be38c940cd6415e3bd0c099b339be28
Andre Bonfatti <andrebnf@gmail.com>
2022-07-29 14:07:45 -0300

    adicionado titulo da pagina

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,6 +27,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Núcleo de Tecnologia - Curso Online</title>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
