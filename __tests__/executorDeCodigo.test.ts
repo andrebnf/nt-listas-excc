36b5165ef0c111a8c461affeb7886be51c0daa42
Andre Bonfatti <andrebnf@gmail.com>
2022-08-10 16:29:36 -0300

    teste tabular para o executor de codigo

--- __tests__/executorDeCodigo.test.ts
+++ __tests__/executorDeCodigo.test.ts
@@ -2,36 +2,66 @@ import {
   executaJavaScript
 } from "../lib/executorDeCodigo"
 
-it('retorna corretamente os logs do código', () => {
-  let codigo = `let x = 1
-  let y = 2
+type InputDeCenario = [
+  codigo: string, 
+  qtdeLogs: number, 
+  deveFalhar: boolean, 
+  linhaDoErro: number,
+  erroEsperado: string,
+  logsEsperados: string[]
+]
+
+// Teste tabular para diferentes cenarios de codigos com ou sem erros
+
+const cenarios: InputDeCenario[] = [
+  // sem erro:
+  [`let x = 10
+  for (let i = x; i > 0; i--) { 
+    console.log(i)
+  }`, 10, false, 0, '', []],
+
+  [`let x = 2
   console.log(x)
-  console.log(y)
-  console.log(\`Soma: \${x + y}\`) 
-  `
-
-  let [logs, temErro, linhaDoErro, mensagemDeErro] = executaJavaScript(codigo)
-  expect(logs.length).toEqual(3)
-  expect(logs).toEqual(['1', '2', 'Soma: 3'])
+  console.log(\`Soma: \${x + 8}\`)
+  `, 2, false, 0, '', ['2', 'Soma: 10']],
   
-  expect(temErro).toBe(false)
-  expect(linhaDoErro).toEqual(0)
-  expect(mensagemDeErro).toEqual('')
-})
+  [`console.log(1 + 1)`, 1, false, 0, '', ['2']],
+  
+  [`function abc(str) {
+    return str + '!'
+  }
+  console.log(abc('oi'))`, 1, false, 0, '', ['oi!']],
+  
+  // com erro:
+  [`console.log(nao_declarada)`, 0, true, 1, 'nao_declarada', []],
 
-it('retorna linha com erro de runtime', () => {
-  let codigo = `console.log(w)`
+  [`for (let i = 0; i < 10; i++) { 
+    console.log(i)
+    console.log(variavel_nao_definida)
+  }`, 1, true, 3, 'variavel_nao_definida', []],
+
+  [`// abc
+  
+  function abc(str) {
+    return str + variavel_nao_definida
+  }
+  console.log(abc('oi'))`, 0, true, 4, 'variavel_nao_definida', []],
+
+]
+
+const fnTesteTabular = test.each<InputDeCenario>(cenarios)
+
+fnTesteTabular('testa diversos pedacos de codigos', (
+  codigo, qtdeLogs, deveFalhar, linhaDoErroEsperada, erroEsperado, logsEsperados
+  ) => {
   let [logs, temErro, linhaDoErro, mensagemDeErro] = executaJavaScript(codigo)
   
-  expect(logs.length).toEqual(0)
-  expect(temErro).toBe(true)
-  expect(linhaDoErro).toEqual(1)
-  expect(mensagemDeErro).toMatch(/w/)
-})
+  expect(logs.length).toEqual(qtdeLogs)
+  expect(temErro).toBe(deveFalhar)
+  expect(linhaDoErro).toEqual(linhaDoErroEsperada)
+  erroEsperado !== '' && expect(mensagemDeErro).toContain(erroEsperado)
 
-// it.each([
-//   [`let x = 10
-//   for (let i = x; i > 0; i--) { 
-//     console.log(i)
-//   }                            `, 10, false, 0],
-// ])
+  if (logsEsperados.length > 0) {
+    expect(logs).toEqual(logsEsperados)
+  }
+});
