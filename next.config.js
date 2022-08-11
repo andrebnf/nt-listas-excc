09e08e3086547e788710ffaebea3e73e41374d8f
Andre Bonfatti <andrebnf@gmail.com>
2022-08-11 14:32:30 -0300

    adicionado html estatico de manutencao

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
-  }
+  },
+  redirects: [
+    {
+      source: "/((?!manutencao).*)",
+      destination: "/manutencao.html",
+      permanent: false
+    }
+  ]
 }
 
 module.exports = nextConfig
